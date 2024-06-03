602ab425ab4065731feddcfb044648f6f81f04fe
Raghul Ajith Nandakumar <96931716+raghulajithn@users.noreply.github.com>
2024-06-03 23:43:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ import streamlit as st
 
 nest_asyncio.apply()
 
+os.system("sudo apt-get install -y libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libatspi2.0-0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libxkbcommon0 libpango-1.0-0 libcairo2 libasound2")
+
 # Install Playwright
 os.system("playwright install")
 
